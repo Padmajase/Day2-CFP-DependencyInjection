4d00242fb96bd71907281c57d133dcb744a64aa9
Padmajase <padmajapawar7@gmail.com>
2022-06-16 22:47:22 -0700

    Added : dependency injection spring concept project

---
+++ springconcept/src/main/java/com/bridgelabz/springconcept/SpringConceptApplication.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.springconcept;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptApplication {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to spring concepts !");
+		SpringApplication.run(SpringConceptApplication.class, args);
+
+	}
+
+}
