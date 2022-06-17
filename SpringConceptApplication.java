fe908c0ced342dc411e3314a53f7202c96eb07c6
Padmajase <padmajapawar7@gmail.com>
2022-06-17 00:18:23 -0700

    Demonstrating : Dependency Injection

--- SpringConceptApplication.java
+++ SpringConceptApplication.java
@@ -1,18 +1,25 @@
 package com.bridgelabz.springconcept;
 
-import org.apache.catalina.core.ApplicationContext;
+import com.bridgelabz.springconcept.component.DemoBean;
+import com.bridgelabz.springconcept.component.EmployeeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptApplication {
 	public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 	public static void main(String[] args) {
 		logger.debug("welcome to spring concepts !");
-		SpringApplication.run(SpringConceptApplication.class, args);
-
+		ApplicationContext context = SpringApplication.run(SpringConceptApplication.class, args);
+		logger.debug("Checking context: {}", context.getBean(DemoBean.class));
+		logger.debug("*** Example using @Autowire annotation on property *** ");
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.seteName("Spring Framework guru ");
+		employeeBean.showEmployeeDetails();
 	}
 
 }
