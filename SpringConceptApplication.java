c9bab5c23e68bc7d99ecf7ba4000170306e15522
Padmajase <padmajapawar7@gmail.com>
2022-06-16 23:36:22 -0700

    Defined : Logger in java file

--- SpringConceptApplication.java
+++ SpringConceptApplication.java
@@ -1,14 +1,16 @@
 package com.bridgelabz.springconcept;
 
 import org.apache.catalina.core.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringConceptApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 	public static void main(String[] args) {
-		System.out.println("welcome to spring concepts !");
+		logger.debug("welcome to spring concepts !");
 		SpringApplication.run(SpringConceptApplication.class, args);
 
 	}
